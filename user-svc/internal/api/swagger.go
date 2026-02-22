f1b27440c43b10c2588fbeb291b7e69bdb51f221
SundayYogurt <kritsanalalu5@gmail.com>
2026-02-22 21:42:34 +0700

    feature(auth): cors fix swagger

--- user-svc/internal/api/swagger.go
+++ user-svc/internal/api/swagger.go
@@ -1,11 +1,21 @@
 package api
 
 import (
-	_ "github.com/SundayYogurt/user_service/docs"
+	docs "github.com/SundayYogurt/user_service/docs" // ใช้ set Host/Schemes
 	"github.com/gofiber/fiber/v2"
 	fiberSwagger "github.com/swaggo/fiber-swagger"
 )
 
 func RegisterSwagger(app *fiber.App) {
+	// set ตอน runtime ให้ตรงโดเมน/โปรโตคอลที่ user เข้า (http หรือ https)
+	app.Use(func(c *fiber.Ctx) error {
+		// Host เช่น "flyup-user-svc.onrender.com"
+		docs.SwaggerInfo.Host = c.Hostname()
+
+		// Protocol: "http" หรือ "https"
+		docs.SwaggerInfo.Schemes = []string{c.Protocol()}
+		return c.Next()
+	})
+
 	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 }
